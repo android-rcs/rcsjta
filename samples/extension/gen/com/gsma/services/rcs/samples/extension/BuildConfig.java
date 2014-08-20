f3dec916a5f3a70a4976246853f6397c0617b7ce
Jean-Marc Auffret <jmauffret@gmail.com>
2014-08-20 10:38:42 +0200

    Update samples TTS and remove binary files

--- samples/extension/gen/com/gsma/services/rcs/samples/extension/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.gsma.services.rcs.samples.extension;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- samples/tts/src/com/orangelabs/rcs/tts/ChatEvent.java
+++ samples/tts/src/com/orangelabs/rcs/tts/ChatEvent.java
@@ -24,9 +24,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.text.TextUtils;
 
 import com.gsma.services.rcs.chat.ChatIntent;
-import com.gsma.services.rcs.chat.ChatMessage;
+import com.gsma.services.rcs.chat.ChatLog;
 
 /**
  * Chat invitation event receiver
@@ -40,16 +43,30 @@ public class ChatEvent extends BroadcastReceiver {
         SharedPreferences preferences = context.getSharedPreferences(Registry.REGISTRY, Activity.MODE_PRIVATE);
         boolean flag = Registry.readBoolean(preferences, Registry.ACTIVATE_TTS, false);
         if (flag) {
-        	// Get the chat message from the Intent
-    		ChatMessage message = intent.getParcelableExtra(ChatIntent.EXTRA_MESSAGE);
+        	// Get the chat message ID from the Intent
+    		String msgId = intent.getParcelableExtra(ChatIntent.EXTRA_MESSAGE_ID);
     		
-			// Play TTS on the chat message
-        	ArrayList<String> messages = new ArrayList<String>();
-			messages.add(context.getString(R.string.label_new_msg));        		
-			messages.add(message.getMessage());        		
-			Intent serviceIntent = new Intent(context, PlayTextToSpeech.class);
-			serviceIntent.putStringArrayListExtra("messages", messages);
-			context.startService(serviceIntent);        	    	
+    		// Get the message content associated to the message ID from the database
+    		String message = null;
+    		Uri uri = ChatLog.Message.CONTENT_URI;
+    	    String[] PROJECTION = new String[] {
+    	    		ChatLog.Message.BODY
+    	    };
+    	    String where = ChatLog.Message.MESSAGE_ID + "='" + msgId + "'";
+    		Cursor cursor = context.getContentResolver().query(uri, PROJECTION, where, null, null);
+    		if ((cursor != null) && (cursor.getCount() > 0)) {
+    			message = cursor.getString(0);
+    		}
+    		
+    		if (!TextUtils.isEmpty(message)) {
+				// Play TTS on the chat message
+	        	ArrayList<String> messages = new ArrayList<String>();
+				messages.add(context.getString(R.string.label_new_msg));        		
+				messages.add(message);        		
+				Intent serviceIntent = new Intent(context, PlayTextToSpeech.class);
+				serviceIntent.putStringArrayListExtra("messages", messages);
+				context.startService(serviceIntent);
+    		}
         }
     }
 }
