f1e824136ede854b6abb216161fd4775e3ead713
yplo6403 <philippe.lemordant@orange.com>
2015-03-23 11:54:56 +0100

    - Uses Configuration#mcc instead of TelephonyManager to retrieve country code - Add trace to ContactUtol#formatContact - Override hashCode and equals methods to RcsContact object

--- core/src/com/gsma/services/rcs/contact/RcsContact.java
+++ core/src/com/gsma/services/rcs/contact/RcsContact.java
@@ -211,4 +211,30 @@ public class RcsContact implements Parcelable {
     public long getBlockingTimestamp() {
         return mBlockingTimestamp;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((mContact == null) ? 0 : mContact.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RcsContact other = (RcsContact) obj;
+        if (mContact == null) {
+            if (other.mContact != null)
+                return false;
+        } else if (!mContact.equals(other.mContact))
+            return false;
+        return true;
+    }
+
 }
