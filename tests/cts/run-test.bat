71043db7b7bf86c54fd576d0933df5c8d7316d18
yplo6403 <philippe.lemordant@orange.com>
2015-05-04 11:05:31 +0200

    Change InstrumentationTestRunner location for running test signature

--- tests/cts/run-test.bat
+++ tests/cts/run-test.bat
@@ -5,7 +5,7 @@ cd provider
 cd ..
 
 cd signature
-%ANDROID_HOME%/platform-tools/adb shell am instrument -w android.tests.sigtest/android.tests.sigtest.InstrumentationRunner
+"%ANDROID_HOME%/platform-tools/adb" shell am instrument -w android.tests.sigtest/android.test.InstrumentationTestRunner
 
 
 pause
