e2ef00038ebe4fe9fe5de9dabe4579135ea939e3
Imre Sunyi <imre.sunyi@sonymobile.com>
2015-05-20 11:59:14 +0200

    Revert "Start the activity manager when receiving 200 OK"
    This reverts commit 576a9817f60e0c727c75b9240a823ac847b7301d.
    
    Only 1-1 chat should have local inactivity timer.
    
    Change-Id: Ib42db5254cedecfaa7ad79eb134baf726f739c6c

--- core/src/com/gsma/rcs/core/ims/service/im/chat/GroupChatSession.java
+++ core/src/com/gsma/rcs/core/ims/service/im/chat/GroupChatSession.java
@@ -638,9 +638,6 @@ public abstract class GroupChatSession extends ChatSession {
         super.handle200OK(resp);
 
         mConferenceSubscriber.subscribe();
-
-        /* Start the activity manager. */
-        getActivityManager().start();
     }
 
     private boolean isDisplayReportRequested(String dispositionNotification) {
