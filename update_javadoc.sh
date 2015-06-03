70b1481c95743dac41fb208082c8615b0fdf1bb6
yplo6403 <philippe.lemordant@orange.com>
2015-06-03 10:45:07 +0200

    Optimize doclava generation: - Control if $ANDROID_TARGET is set
    Refactor script to push new release of API javadoc:
    1- Check if lavadoc is generated before pushing it
    2- Fetch rcsjta.javadoc repository
    3- Select javadoc1.5 branch
    4- Pull javadoc1.5 from remote
    
    Add doclava to .gitignore

--- update_javadoc.sh
+++ update_javadoc.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/sh
-if [ -d "$GIT_HOME/rcsjta/doclava" ]; then
+if [ -d "$GIT_HOME/rcsjta/doclava/reference" ]; then
 	if [ -d "$GIT_HOME/rcsjta.javadoc" ]; then
-		cd $GIT_HOME/rcsjta.javadoc
+		cd "$GIT_HOME/rcsjta.javadoc"
+		git fetch origin
+		git checkout javadoc1.5
+		git pull origin javadoc1.5
 		git reset --hard 9d755aa6730a89fa054b38028eab1c064379ed2a
-		rm -Rf $GIT_HOME/rcsjta.javadoc/reference 2> /dev/null
-		cp -R $GIT_HOME/rcsjta/doclava/reference $GIT_HOME/rcsjta.javadoc
+		rm -Rf "$GIT_HOME/rcsjta.javadoc/reference" 2> /dev/null
+		cp -R "$GIT_HOME/rcsjta/doclava/reference" "$GIT_HOME/rcsjta.javadoc"
 		git add --all
 		git commit -m "Generate javadoc 1.5"
-		git push -f origin master:javadoc1.5
+		git push -f origin javadoc1.5
 		echo "Doclava created for release 1.5"
 	else
 		echo "First clone rcsjta.javadoc repository"
 	fi
 else
-	echo "First create doclava for release 1.5"
+	echo "First create doclava for release 1.5 by issuing command \"ant docs\""
 fi
