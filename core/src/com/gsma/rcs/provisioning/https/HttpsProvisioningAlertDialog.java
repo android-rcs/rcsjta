5c32d1cea23cb962128f73b1617cb2b988ddf7e4
yplo6403 <philippe.lemordant@orange.com>
2015-06-18 17:21:53 +0200

    Issue 113: WiFi provisioning: unfriendly user interface - disable cancel outside alert dialog - Use entryType="phone" for MSISDN - Refactor the HttpsProvisioningAlertDialog activity as an alertDialog with no background window

--- core/src/com/gsma/rcs/provisioning/https/HttpsProvisioningAlertDialog.java
+++ core/src/com/gsma/rcs/provisioning/https/HttpsProvisioningAlertDialog.java
@@ -21,16 +21,18 @@ package com.gsma.rcs.provisioning.https;
 import com.gsma.rcs.R;
 import com.gsma.rcs.utils.ContactUtil;
 import com.gsma.rcs.utils.ContactUtil.PhoneNumber;
+import com.gsma.rcs.utils.logger.Logger;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.CountDownTimer;
 import android.view.KeyEvent;
-import android.widget.EditText;
-
-import java.lang.ref.WeakReference;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * HTTPS provisioning - MSISDN Pop-up activity
@@ -39,27 +41,32 @@ import java.lang.ref.WeakReference;
  */
 public class HttpsProvisioningAlertDialog extends Activity {
 
-    private AlertDialog mAlertDialog;
+    private static final long COUNTDOWN_INTERVAL_MSEC = 1000;
+    
     private AlertDialog mErrorDialog;
-    private AutoDismissRunnable mDialogRunnable;
-    private Handler mDialogHandler;
+    
+    private CountDownTimer mCountDownTimer;
+    
+    private static final Logger sLogger = Logger.getLogger(HttpsProvisioningAlertDialog.class.getSimpleName());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.rcs_wifi_provisioning);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.label_edit_msisdn);
+        Button ok_button = (Button) findViewById(R.id.ok_button);
 
-        // Set an EditText view to get user input
-        final EditText input = new EditText(this);
-        builder.setView(input);
+        final TextView msisdn = (TextView) findViewById(R.id.msisdn);
 
-        builder.setPositiveButton(R.string.label_ok, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                PhoneNumber phoneNumber = ContactUtil.getValidPhoneNumberFromAndroid(input
-                        .getText().toString());
+        ok_button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String contact = msisdn.getText().toString();
+                PhoneNumber phoneNumber = ContactUtil.getValidPhoneNumberFromAndroid(contact);
                 if (phoneNumber == null) {
+                    if (sLogger.isActivated())  {
+                        sLogger.warn("User entered a wrong MSISDN '"+contact+"'!");
+                    }
                     mErrorDialog = new AlertDialog.Builder(HttpsProvisioningAlertDialog.this)
                             .setTitle(R.string.label_invalid_phone_number)
                             .setPositiveButton(R.string.label_ok,
@@ -67,10 +74,14 @@ public class HttpsProvisioningAlertDialog extends Activity {
                                         public void onClick(DialogInterface dialog, int whichButton) {
                                             mErrorDialog.dismiss();
                                             mErrorDialog = null;
-                                            mAlertDialog.show();
                                         }
-                                    }).show();
+                                    }).create();
+                    mErrorDialog.setCanceledOnTouchOutside(false);
+                    mErrorDialog.show();
                 } else {
+                    if (sLogger.isActivated())  {
+                        sLogger.debug("User entered MSISDN ".concat(phoneNumber.getNumber()));
+                    }
                     HttpsProvionningMSISDNInput.getInstance().responseReceived(
                             ContactUtil.createContactIdFromValidatedData(phoneNumber));
                     finish();
@@ -78,63 +89,58 @@ public class HttpsProvisioningAlertDialog extends Activity {
             }
         });
 
-        builder.setNegativeButton(R.string.label_cancel, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
+        Button cancel_button = (Button) findViewById(R.id.cancel_button);
+        cancel_button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sLogger.isActivated())  {
+                    sLogger.warn("User cancelled the MSISDN dialog!");
+                }
                 HttpsProvionningMSISDNInput.getInstance().responseReceived(null);
                 finish();
             }
         });
-
-        mAlertDialog = builder.show();
-        mAlertDialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+        mCountDownTimer = new CountDownTimer(HttpsProvisioningUtils.INPUT_MSISDN_TIMEOUT, COUNTDOWN_INTERVAL_MSEC) {
 
             @Override
-            public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_BACK) {
-                    mAlertDialog.dismiss();
-                    finish();
-                    return true;
-                }
-                return false;
+            public void onTick(long millisUntilFinished) {
             }
-        });
-        mDialogRunnable = new AutoDismissRunnable(this);
 
-        mDialogHandler = new Handler();
+            @Override
+            public void onFinish() {
+                if (sLogger.isActivated())  {
+                    sLogger.warn("MSISDN dialog has expired!");
+                }
+                HttpsProvionningMSISDNInput.getInstance().responseReceived(null);
+                mCountDownTimer = null;
+                finish();
+            }
+        }.start();
+    }
 
-        mDialogHandler.postDelayed(mDialogRunnable, HttpsProvisioningUtils.INPUT_MSISDN_TIMEOUT);
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                if (sLogger.isActivated())  {
+                    sLogger.warn("User exited the MSISDN dialog!");
+                }
+                HttpsProvionningMSISDNInput.getInstance().responseReceived(null);
+                finish();
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
     protected void onDestroy() {
-
-        mDialogHandler.removeCallbacks(mDialogRunnable);
-
-        mAlertDialog.dismiss();
-
+        if (mErrorDialog != null) {
+            mErrorDialog.dismiss();
+        }
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+        }
         super.onDestroy();
     }
 
-    private class AutoDismissRunnable implements Runnable {
-
-        WeakReference<HttpsProvisioningAlertDialog> activityWeak;
-
-        public AutoDismissRunnable(HttpsProvisioningAlertDialog activity) {
-            activityWeak = new WeakReference<HttpsProvisioningAlertDialog>(activity);
-        }
-
-        public void run() {
-            HttpsProvionningMSISDNInput.getInstance().responseReceived(null);
-
-            HttpsProvisioningAlertDialog activity = activityWeak.get();
-
-            if (activity != null) {
-                if (mErrorDialog != null) {
-                    mErrorDialog.dismiss();
-                }
-                activity.mAlertDialog.dismiss();
-                activity.finish();
-            }
-        }
-    }
 }
