5e3192c406301fc07010593785f6339e6847536b
Vadapalli Murthy <vadapalli.murthy@sonymobile.com>
2015-09-15 16:05:57 +0200

    Exception handling update for OneToOneDeliveryExpirationService
    Changes :
    - Handled runtime error conditions.
    - Moved the service to background execution.
    
    Change-Id: Ifdec229896e8a7e57775f4b57e2db5f2f2ceb447
    Signed-off-by: Kristoffer Karlsson <kristoffer.karlsson2@sonymobile.com>

--- core/src/com/gsma/rcs/service/api/OneToOneDeliveryExpirationService.java
+++ core/src/com/gsma/rcs/service/api/OneToOneDeliveryExpirationService.java
@@ -24,28 +24,48 @@ import android.content.Intent;
 
 public class OneToOneDeliveryExpirationService extends IntentService {
 
-    private final Logger mLogger = Logger.getLogger(getClass().getName());
+    private final Logger sLogger = Logger.getLogger(OneToOneDeliveryExpirationService.class
+            .getName());
 
     public OneToOneDeliveryExpirationService() {
         super(OneToOneDeliveryExpirationService.class.getCanonicalName());
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
-        Core core = Core.getInstance();
+    protected void onHandleIntent(final Intent intent) {
+        final Core core = Core.getInstance();
         if (core == null || !core.isStarted()) {
             /* Stack is not started, don't process this intent */
-            if (mLogger.isActivated()) {
-                mLogger.warn("Stack is not running, do not process the delivery expiration intent!");
+            if (sLogger.isActivated()) {
+                sLogger.warn("Stack is not running, do not process the delivery expiration intent!");
             }
             return;
         }
-        String action = intent.getAction();
-        if (OneToOneUndeliveredImManager.ACTION_CHAT_MESSAGE_DELIVERY_TIMEOUT.equals(action)) {
-            core.getListener().handleOneToOneChatMessageDeliveryExpiration(intent);
-        } else if (OneToOneUndeliveredImManager.ACTION_FILE_TRANSFER_DELIVERY_TIMEOUT
-                .equals(action)) {
-            core.getListener().handleOneToOneFileTransferDeliveryExpiration(intent);
-        }
+        core.scheduleForBackgroundExecution(new Runnable() {
+            @Override
+            public void run() {
+                String action = intent.getAction();
+                try {
+                    if (OneToOneUndeliveredImManager.ACTION_CHAT_MESSAGE_DELIVERY_TIMEOUT
+                            .equals(action)) {
+                        core.getListener().handleOneToOneChatMessageDeliveryExpiration(intent);
+                    } else if (OneToOneUndeliveredImManager.ACTION_FILE_TRANSFER_DELIVERY_TIMEOUT
+                            .equals(action)) {
+                        core.getListener().handleOneToOneFileTransferDeliveryExpiration(intent);
+                    }
+                } catch (RuntimeException e) {
+                    /*
+                     * Normally we are not allowed to catch runtime exceptions as these are genuine
+                     * bugs which should be handled/fixed within the code. However the cases when we
+                     * are executing operations on a thread unhandling such exceptions will
+                     * eventually lead to exit the system and thus can bring the whole system down,
+                     * which is not intended.
+                     */
+                    sLogger.error(
+                            "Unable to handle one to one delivery expiration event for intent action : "
+                                    .concat(action), e);
+                }
+            }
+        });
     }
 }
