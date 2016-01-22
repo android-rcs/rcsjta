d2294f81ade1d30ebfdd3b87aa5ae3b0359b2b12
jmauffret <jmauffret@gmail.com>
2016-01-22 08:16:26 +0100

    Javadoc

--- core/src/com/gsma/rcs/core/ims/service/sip/EnrichCallingService.java
+++ core/src/com/gsma/rcs/core/ims/service/sip/EnrichCallingService.java
@@ -3,19 +3,34 @@ package com.gsma.rcs.core.ims.service.sip;
 import com.gsma.rcs.core.ims.network.sip.FeatureTags;
 
 /**
- * Enrich calling service
+ * Enrich calling service implemented on top of the MM session service
  *
  * @author jmauffret
  */
 public class EnrichCallingService {
+    /**
+     * Call composer service ID
+     */
     public final static String CALL_COMPOSER_SERVICE_ID = "gsma.callcomposer";
 
+    /**
+     * Shared map service ID
+     */
     public final static String SHARED_MAP_SERVICE_ID = "gsma.sharedmap";
 
+    /**
+     * Shared sketch service ID
+     */
     public final static String SHARED_SKETCH_SERVICE_ID = "gsma.sharedsketch";
 
+    /**
+     * Post call service ID
+     */
     public final static String POST_CALL_SERVICE_ID = "gsma.callunanswered";
 
+    /**
+     * Call composer feature tag
+     */
     public final static String CALL_COMPOSER_FEATURE_TAG = FeatureTags.FEATURE_RCSE + "=\""
             + FeatureTags.FEATURE_RCSE_IARI_EXTENSION + "." + CALL_COMPOSER_SERVICE_ID + "\"";
 }
